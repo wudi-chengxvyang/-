db0a3181ac1b485d34956c6e84963fb7204cbc45
mayunfei <853602380@qq.com>
2021-03-30 19:06:06 +0800

    第二次提交

--- src/main/java/com/example/domain/Student.java
+++ src/main/java/com/example/domain/Student.java
@@ -9,4 +9,50 @@ public class Student {
     private String age;
     private String school;
     private String name01;
+
+    public String getName() {
+        return name;
+    }
+
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public Student setAge(String age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public Student setSchool(String school) {
+        this.school = school;
+        return this;
+    }
+
+    public String getName01() {
+        return name01;
+    }
+
+    public Student setName01(String name01) {
+        this.name01 = name01;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", school='" + school + '\'' +
+                ", name01='" + name01 + '\'' +
+                '}';
+    }
 }
